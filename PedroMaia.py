9a2c113ecff71f95f6af5f1f98c40e39894cd138
pedrocmmaia <121133422+pedrocmmaia@users.noreply.github.com>
2024-08-23 13:07:30 +0200

    Delete PedroMaia.py

--- PedroMaia.py
+++
@@ -1,9 +0,0 @@
-with open('arquivo.txt', 'w') as file:
-    # Escreve algumas linhas no arquivo
-    file.write("Nome: Pedro Maia\n")
-    file.write("Idade: 18 anos\n")
-    file.write("Motivação: Estudante do curso de SI, pelo interesse na area de tecnologia\n")
-
-with open('arquivo.txt', 'r') as file:
-    conteudo = file.read()
-    print(conteudo)
